c4b49a27c376ba3a33413af8e318b5042d4a738c
I <kibayamuhia@gmail.com>
2024-06-14 09:44:57 +0300

    SQL practice

---
+++ SQL Practice/Question 9.sql
@@ -0,0 +1,10 @@
+create table orders(order_id int, customer_id int, order_date date)
+
+insert into orders values(2043, 1, '2023-06-01'),
+							(2044, 2, '2023-06-02'),
+							(2045, 3, '2023-06-03'),
+							(2046, 1, '2023-06-04')
+
+BEGIN TRANSACTION
+update orders set order_date = '2023-07-23' where order_id = 2045
+COMMIT TRANSACTION
